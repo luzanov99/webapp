e7c9c78afca0c1a6abbf7652ed5fb53982ddc9ed
Luazanov Yevgen <luzanov.zena@gmail.com>
2020-12-16 12:04:51 +0200

    Поддержка базы данных

--- webapp/python_org_news.py
+++ webapp/python_org_news.py
@@ -1,5 +1,7 @@
 import requests
+from datetime import datetime
 from bs4 import BeautifulSoup
+from webapp.model import db, News
 
 def get_html(url):
     try:
@@ -10,30 +12,29 @@ def get_html(url):
         print('Сетевая ошибка')
         return False
 
+
+
 def get_python_news():
     html=get_html("https://www.python.org/blogs/")
     if html:
         soup=BeautifulSoup(html, 'html.parser')
         all_news=soup.find('ul', class_='list-recent-posts').findAll('li')
         
-        result_news=[]
         for news in all_news:
-            try:
-                title = news.find('a').text 
-                url=news.find('a')['href']
-                published=news.find('time').text
-                result_news.append({
-                'title':title,
-                'url':url,
-                'published':published
-                })
             
-            except:
-                print("Error")
-        return result_news
-    
-    return False
-    
-        
-       
-
+            title = news.find('a').text 
+            url=news.find('a')['href']
+            published=news.find('time').text
+            try:
+                published=datetime.strptime(published, '%Y-%m-%d')
+            except(ValueError):
+                published=datetime.now()
+                save_news(title, url, published)
+            
+def save_news(title ,url,published):
+    news_exists=News.query.filter(News.url ==url).count()
+    print(news_exists)
+    if not news_exists:
+        news_news=News(title=title, url=url, published=published)
+        db.session.add(news_news)
+        db.session.commit()
